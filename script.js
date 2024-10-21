340311d96323c142cf294b90900e0e82db16419d
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-10-21 13:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  let trueStr = s1.toLowerCase();
+  let trueSubStr = s2.toLowerCase();
+
+  if (trueStr.includes(trueSubStr)) {
+    for (let i = 0; i < trueStr.length; i++) {
+      // Make pairs
+      let pairs = ""
+      for (let p = i; p < trueSubStr.length + i; p++) {
+        pairs += trueStr[p]
+      }
+      console.log(pairs,trueSubStr);
+      
+      if (pairs == trueSubStr) return i
+    }
+  }
+  return -1;
 }
 
 // Please do not change the code below
